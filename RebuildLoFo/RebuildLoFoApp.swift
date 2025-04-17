57f5b08ce9dafc46559b255476cf381fd6c9e3af
Nicholas <nicholaschao1507@gmail.com>
2025-04-17 16:46:56 +0700

    Initial Commit

---
+++ RebuildLoFo/RebuildLoFoApp.swift
@@ -0,0 +1,32 @@
+//
+//  RebuildLoFoApp.swift
+//  RebuildLoFo
+//
+//  Created by Nicholas  on 17/04/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RebuildLoFoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
